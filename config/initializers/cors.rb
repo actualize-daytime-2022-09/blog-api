154d479821acabf552922c1a87cfbd9980d3044e
Connor La Rocca <connorlarocca@hotmail.com>
2022-12-22 14:51:44 -0500

    changing site cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "your-frontend-app-name.netlify.app"
+    origins "localhost:8080", "flourishing-peony-356570.netlify.app"
 
     resource "*",
       headers: :any,
