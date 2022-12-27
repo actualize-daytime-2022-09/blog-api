932e6eb67411c14c934b2a812f22f02323d0adb1
Sara Gluck <srfgglck@gmail.com>
2022-12-27 14:38:52 -0500

    Adds test for specfic ID

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Posts", type: :request do
+  # This test is checking the length of my array of posts.
   describe "GET /posts" do
     it "should load all my posts" do
       user = User.create!(name: "Sara", email: "sara@test.com", password: "password")
@@ -15,6 +16,18 @@ RSpec.describe "Posts", type: :request do
       expect(posts.length).to eq(4)
     end
   end
+  #This tests if I can get something specific based on ID and return the right info
+  describe "GET /posts/:id" do
+    it "should pull up a specifc posts based on the id param" do
+      user = User.create!(name: "Sara", email: "sara@test.com", password: "password")
+      Post.create!(title: "Post", body: "text", image: "sample", user_id: user.id)
+      post_id = Post.first.id
+      get "/posts/#{post_id}.json"
+      post = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(post["title"]).to eq("Post")
+      expect(post["body"]).to eq("text")
+      expect(post["image"]).to eq("sample")
+    end
+  end
 end
-
-# This test is checking the length of my array of posts.
