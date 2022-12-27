4f38429c885a351fb5f13e28183a331b76a7042c
Sara Gluck <srfgglck@gmail.com>
2022-12-27 13:57:03 -0500

    Works?

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_index_path
+    it "should load all my posts" do
+      user = User.create!(name: "Sara", email: "sara@test.com", password: "password")
+      Post.create!(title: "Post", body: "text", image: "sample", user_id: user.id)
+      Post.create!(title: "Post", body: "text", image: "sample", user_id: user.id)
+      Post.create!(title: "Post", body: "text", image: "sample", user_id: user.id)
+      Post.create!(title: "Post", body: "text", image: "sample", user_id: user.id)
+
+      get "/posts.json"
+      posts = JSON.parse(response.body)
       expect(response).to have_http_status(200)
+      expect(posts.length).to eq(4)
     end
   end
 end
+
+# This test is checking the length of my array of posts.
